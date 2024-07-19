95ee98be817acdc7cf744be8b56143a6fc03fdfc
yamakoud <kyamad9@gmail.com>
2024-07-19 09:10:52 +0900

    Add instruction 3

---
+++ src/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+int main() {
+    std::string input;
+
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, input);
+
+    // TODO: Implement string reversal
+
+    // TODO: Implement vowel counting
+
+    return 0;
+}
